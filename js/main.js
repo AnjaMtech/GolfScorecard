657f347c877139c5e451dbb7124e9c8ab34ec7d2
Anja Schmidt <anjaschmidt@Anjas-MacBook-Air.local>
2022-04-08 10:33:36 -0600

    null

--- js/main.js
+++ js/main.js
@@ -1,8 +1,5 @@
-// function getAvailableCourses() {
-//  return fetch('https://golf-courses-api.herokuapp.com/courses/').then(
-//    log();
-//  );
-// }
+
+let screenMode = "options";
 let myCourses;
 
 function log(input){
@@ -10,23 +7,46 @@ function log(input){
   console.log(input)
 }
 
+function print(){
+  if(screenMode == "options"){
+    log("my courses is")
+    log(myCourses);
+    log('screenMode is options')
+    $("#options-container").empty();
+    for(let i=0; i<myCourses.length; i++){
+      $("#options-container").append(
+        `<div class="img-box" onclick="golfCourse('${myCourses[i].name}')">
+          <img src="${myCourses[i].image}" alt="${myCourses[i].name}">
+          <div class="img-overlay"><span>${myCourses[i].name}</span></div>
+        </div>`
+      );
+    }
+  }
+}
 
 function getApi() {
     return fetch('https://golf-courses-api.herokuapp.com/courses')
     .then(response => response.json())
-    // .then(data => log(data.courses));
-    .then(data => myCourses = data.courses);
+    .then(data => {myCourses = data.courses; print()})
 }
 getApi();
 
 
-window.onload = function(){
-  console.log("loaded")
-  log(myCourses);
+function golfCourse(input){
+  log(input);
+  screenMode="scorecard";
+  print();
+}
 
+
+window.onload = function(){
+  log("Window has loaded")
+  log(myCourses);
+  // print();
   document.body.onkeypress = function(e){
     if(e.keyCode === 32){
-      log(myCourses);
+      log(myCourses[0].name);
+      // print();
     }
   }
 }
